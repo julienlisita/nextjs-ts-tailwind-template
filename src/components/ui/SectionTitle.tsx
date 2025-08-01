4b767bc54895e2712733dcbbcb4126f5e99c8059
Julien Lisita <julien.lisita@gmail.com>
2025-08-01 22:26:43 +0200

    feat: add SectionTitle component

---
+++ src/components/ui/SectionTitle.tsx
@@ -0,0 +1,30 @@
+// src/components/ui/SectionTitle.tsx
+
+'use client';
+
+import React from "react";
+import { resolveColor } from "@/lib/resolveColor";
+
+type SectionTitleProps = {
+  id?: string;
+  children: React.ReactNode;
+  className?: string;
+  color?: string;
+};
+
+export default function SectionTitle({
+  id,
+  children,
+  className = "",
+  color = "inherit",
+}: SectionTitleProps) {
+  return (
+    <h2
+      id={id}
+      className={`text-xl sm:text-2xl lg:text-3xl font-heading text-center font-semibold ${className}`}
+      style={{ color: resolveColor(color) }}
+    >
+      {children}
+    </h2>
+  );
+}
