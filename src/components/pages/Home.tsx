09e9e3739d6137a69d3494e186565ca9570e31e7
Julien Lisita <julien.lisita@gmail.com>
2025-08-05 00:16:21 +0200

    feat: add base Hero section component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,11 @@
 // src/components/pages/Home.tsx
 
-import PageTitle from "@/components/ui/PageTitle"
-import Button from "@/components/ui/Button";
+import Hero from "../section/Hero";
 
 export default function Home() {
   return (
     <div className="py-20 text-center">
-      <PageTitle>Bienvenue sur notre site</PageTitle>
-      <p className="text-lg text-gray-600 mb-6">
-        Découvrez nos services et notre expertise.
-      </p>
-      <Button href="/about">En savoir plus</Button>
+      <Hero/>
     </div>
   );
 }
