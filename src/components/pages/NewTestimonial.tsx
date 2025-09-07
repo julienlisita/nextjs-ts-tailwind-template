2b38ff8f8117b1a308e84e0915eeb00d1a83bdf0
Julien Lisita <julien.lisita@gmail.com>
2025-09-07 19:22:47 +0200

    chore: apply prettier formatting on newTestimonial component

--- src/components/pages/NewTestimonial.tsx
+++ src/components/pages/NewTestimonial.tsx
@@ -1,6 +1,6 @@
 // src/components/pages/NewTestimonials.tsx
 
-import { PublicTestimonialForm } from "../form/Form";
+import { NewTestimonialForm } from '../form/NewTestimonialForm';
 
 export const metadata = { title: 'Laisser un témoignage' };
 
@@ -9,7 +9,7 @@ export default function NewTestimonial() {
     <div className="max-w-2xl mx-auto p-6 space-y-6">
       <h1 className="text-2xl font-bold">Laisser un témoignage</h1>
       <p className="text-sm text-gray-600">Merci ! Votre témoignage sera relu avant publication.</p>
-      <PublicTestimonialForm />
+      <NewTestimonialForm />
     </div>
   );
 }
