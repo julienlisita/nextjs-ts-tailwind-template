c44d17e5dd57c707399d54573d2cba8c340e035c
Julien Lisita <julien.lisita@gmail.com>
2025-09-11 11:49:04 +0200

    feat(ui/SectionTitle): add align prop to control text alignment

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -4,12 +4,14 @@
 
 import React from 'react';
 import { resolveColor } from '@/lib/resolveColor';
+import clsx from 'clsx';
 
 type SectionTitleProps = {
   id?: string;
   children: React.ReactNode;
   className?: string;
   color?: string;
+  align?: 'left' | 'center' | 'right';
 };
 
 export default function SectionTitle({
@@ -17,11 +19,18 @@ export default function SectionTitle({
   children,
   className = '',
   color = 'inherit',
+  align = 'center',
 }: SectionTitleProps) {
   return (
     <h2
       id={id}
-      className={`text-xl sm:text-2xl lg:text-3xl font-heading text-center font-semibold ${className}`}
+      className={clsx(
+        `text-xl sm:text-2xl lg:text-3xl font-heading  font-semibold',
+        align === 'left' && 'text-left',
+        align === 'center' && 'text-center',
+        align === 'right' && 'text-right', 
+        className,`
+      )}
       style={{ color: resolveColor(color) }}
     >
       {children}
