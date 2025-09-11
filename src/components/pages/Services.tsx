53a48b45e3c1945c93699cbeb521a911eefc70c0
Julien Lisita <julien.lisita@gmail.com>
2025-09-11 12:50:05 +0200

    refactor(services): replace ServiceCard with FeatureCard and remove old component

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -1,28 +1,37 @@
 // src/components/pages/Services.tsx
 
 import PageTitle from '@/components/ui/PageTitle';
-import ServiceCard from '@/components/widgets/ServiceCard';
 import Cta from '../ui/Cta';
+import FeatureCard from '../widgets/FeaturedCard';
 
 export default function Services() {
   return (
     <div className="pt-8 sm:pt-10 md:pt-14 lg:pt-20">
       <PageTitle>Nos services</PageTitle>
       <section className="flex flex-wrap justify-center gap-6 py-12 px-4">
-        <ServiceCard
+        <FeatureCard
           title="Service 1"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae diam eget orci semper ullamcorper ullamcorper at eros. Sed pretium sed augue id pretium."
-          color={['#60A5FA', '#3B82F6']}
+          variant="with-header"
+          width="w-72"
+          height="h-80"
+          gradient={['#60A5FA', '#3B82F6']}
         />
-        <ServiceCard
+        <FeatureCard
           title="Service 2"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae diam eget orci semper ullamcorper ullamcorper at eros. Sed pretium sed augue id pretium."
-          color={['#60A5FA', '#3B82F6']}
+          variant="with-header"
+          width="w-72"
+          height="h-80"
+          gradient={['#60A5FA', '#3B82F6']}
         />
-        <ServiceCard
+        <FeatureCard
           title="Service 3"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae diam eget orci semper ullamcorper ullamcorper at eros. Sed pretium sed augue id pretium."
-          color={['#60A5FA', '#3B82F6']}
+          variant="with-header"
+          width="w-72"
+          height="h-80"
+          gradient={['#60A5FA', '#3B82F6']}
         />
       </section>
       <Cta />
