5cd1d1e8609145a1d44fdfcb65b37e4b795dfa33
Julien Lisita <julien.lisita@gmail.com>
2025-11-22 15:34:56 +0100

    chore(auth): fix authCookieOptions for Vercel production compatibility

--- src/lib/auth-cookies.ts
+++ src/lib/auth-cookies.ts
@@ -6,6 +6,10 @@ export const authCookieOptions = {
   httpOnly: true,
   path: '/',
   maxAge: 60 * 60 * 24 * 7, // 7 jours
-  sameSite: 'lax' as const,
   secure: process.env.NODE_ENV === 'production',
+  sameSite: 'strict' as const,
+  domain:
+    process.env.NODE_ENV === 'production'
+      ? 'nextjs-ts-tailwind-template-dusky.vercel.app'
+      : 'localhost',
 };
